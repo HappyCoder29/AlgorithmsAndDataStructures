4a9542ac439ca8f3e1bb43b42d94858c23932862
Ashish <ashish@drillmaps.com>
2021-11-29 11:37:23 -0800

    Checking in Code for Flattening a tree.

--- BinaryTreeQuestions/ConvertBinaryTreeToDoubleLinkList/src/edu/northeastern/ashish/BinaryTree.java
+++ BinaryTreeQuestions/ConvertBinaryTreeToDoubleLinkList/src/edu/northeastern/ashish/BinaryTree.java
@@ -22,7 +22,38 @@ public class BinaryTree <T> {
         return  1 + size(node.left) + size(node.right);
     }
 
-    public void levelOrder(){
+    public void flattenTreeInPreOrder(){
+        if(root == null){
+            return;
+        }
+        LinkedList< Node<T> > list = new LinkedList<>();
+        BoxValue box = new BoxValue(0);
+        flattenTreeInPreOrder(root, list, box);
+        Node<T> prev = null;
+        for(int i = 0 ; i < list.size(); i ++){
+            list.get(i).left = prev;
+            if(i +1 != list.size()){
+                list.get(i).right = list.get(i+1);
+            }else{
+                list.get(i).right = null;
+            }
+            prev = list.get(i);
+        }
+        this.root = list.get(0);
+        System.out.println();
+    }
+
+    private void flattenTreeInPreOrder(Node<T> node, LinkedList<Node<T>> list, BoxValue box){
+        if(node != null){
+            list.add(box.value, node);
+            box.value ++;
+            flattenTreeInPreOrder(node.left, list, box);
+            flattenTreeInPreOrder(node.right, list, box);
+        }
+    }
+
+
+    public void flattenTreeInlevelOrder(){
         if(root == null){
             return;
         }
--- BinaryTreeQuestions/ConvertBinaryTreeToDoubleLinkList/src/edu/northeastern/ashish/Main.java
+++ BinaryTreeQuestions/ConvertBinaryTreeToDoubleLinkList/src/edu/northeastern/ashish/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         BinaryTree<Integer> tree = new BinaryTree<>();
         tree.root = createTree();
-        tree.levelOrder();
+        tree.flattenTreeInPreOrder();
         System.out.println();
     }
 
