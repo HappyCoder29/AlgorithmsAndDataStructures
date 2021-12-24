f077bd737146e7a65d3a212122b09388e215e51d
Ashish <ashish@drillmaps.com>
2021-12-24 10:08:21 -0800

    Checking in code for Search in 2d sorted matrix

--- MatrixQuestions/SearchIn2DSortedmatrix/src/edu/northeastern/ashish/Main.java
+++ MatrixQuestions/SearchIn2DSortedmatrix/src/edu/northeastern/ashish/Main.java
@@ -5,13 +5,12 @@ public class Main {
     public static void main(String[] args) {
         int[][] matrix = {{1,3,5,7},{10,11,16,20},{23,30,34,60}};
 
-        System.out.println(search2dSorted(matrix, 54));
+        System.out.println(search2dSorted(matrix, 27));
     }
 
     private static boolean search2dSorted(int[][] matrix, int target){
 
         int rows = matrix.length;
-        int cols = matrix[0].length;
         int row = 0;
         int col = matrix[0].length -1;
 
