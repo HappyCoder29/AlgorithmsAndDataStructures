6567b310a65244b571fe0e73cfb0f586916c27bd
Ashish <ashish@drillmaps.com>
2021-12-24 10:05:35 -0800

    Checking in code for Search in 2d sorted matrix

---
+++ MatrixQuestions/WordSearch/src/edu/northeastern/ashish/Main.java
@@ -0,0 +1,28 @@
+package edu.northeastern.ashish;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+
+    private static boolean searchWordInMatrix(String word, String[][] matrix){
+
+        for(int i = 0 ; i < matrix.length; i ++){
+            for(int j = 0 ; j < matrix[0].length; j ++){
+                if()
+            }
+        }
+
+    }
+    private static boolean searchWordInMatrix(String str, String[][] matrix, boolean[][] visited, int row, int col){
+
+        if()
+
+
+
+    }
+
+
+
+}
